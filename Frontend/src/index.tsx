82091c3406a6b353ec8552f6c996eaeccb61d562
Velo248 <jhpark4884@gmail.com>
2022-11-27 03:29:32 +0900

    feat (msw) : 테스트용 mock api

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -3,6 +3,11 @@ import './styles/tailwind.css';
 import App from './App';
 import React from 'react';
 
+import { worker } from './mocks/browser';
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
