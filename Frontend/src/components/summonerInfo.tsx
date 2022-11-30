a9bcdd2bd33563c167f736d2fb72c115b162c426
LucetTin5 <lucettin5@gmail.com>
2022-11-30 18:00:42 +0900

    fix: 동일부분 Component화

--- Frontend/src/components/summonerInfo.tsx
+++ Frontend/src/components/summonerInfo.tsx
@@ -5,6 +5,25 @@ type Summoner = {
   rank: string;
   tier: string;
 };
+interface IRankType {
+  type: string;
+  tier: string;
+  rank: string;
+}
+const RankType = ({ type, tier, rank }: IRankType) => {
+  return (
+    <div className="flex flex-col text-center basis-1/2">
+      <div className="text-slate-400 text-sm">{type}</div>
+      <div className="mx-auto mt-3 w-1/3 p-1 bg-[#18181d] rounded-full">
+        <img className="h-full w-full object-cover" src={getRankEmblem(tier)} alt="emblem" />
+      </div>
+      <div className="text-white text-sm">
+        {tier} {rank}
+      </div>
+      <div className="mt-3 text-slate-400 text-sm">0승 0패 0%</div>
+    </div>
+  );
+};
 
 const SummonerInfo = ({ name, rank, tier }: Summoner) => {
   return (
@@ -23,28 +42,9 @@ const SummonerInfo = ({ name, rank, tier }: Summoner) => {
         <button className=" text-white text-sm bg-blue-500 rounded-md h-10">지지</button>
       </div>
       <div className="flex mt-5 justify-around">
-        <div className="flex flex-col text-center basis-1/2">
-          <div className="text-slate-400 text-sm">솔로랭크</div>
-          <div className="mx-auto mt-3 w-1/3 p-1 bg-[#18181d] rounded-full">
-            <img className="h-full w-full object-cover" src={getRankEmblem(tier)} alt="emblem" />
-          </div>
-          <div className="text-white text-sm">
-            {tier} {rank}
-          </div>
-          <div className="mt-3 text-slate-400 text-sm">0승 0패 0%</div>
-        </div>
+        <RankType type={'솔로랭크'} tier={tier} rank={rank} />
         <div className="text-center text-slate-400 border-solid border border-black"></div>
-        <div className="flex flex-col text-center basis-1/2">
-          <div className="text-slate-400 text-sm">자유랭크</div>
-          <div className="mx-auto mt-3 w-1/3 p-1 bg-[#18181d] rounded-full">
-            <img
-              className="h-full w-full object-cover"
-              src={getRankEmblem('Master')}
-              alt="emblem"
-            />
-          </div>
-          <div className="mt-3 text-slate-400 text-sm">0승 0패 0%</div>
-        </div>
+        <RankType type={'자유랭크'} tier={'Platinum'} rank={'2'} />
       </div>
     </div>
   );
