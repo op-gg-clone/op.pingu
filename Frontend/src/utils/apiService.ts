90ced2d6553821e4e542b3eed5b771f8d12f1c22
Sunghum Paik <zerosial@gmail.com>
2022-12-01 02:05:12 +0900

    feat: query 추가 , 쓸모없는 파일 삭제

--- Frontend/src/utils/apiService.ts
+++ Frontend/src/utils/apiService.ts
@@ -6,6 +6,7 @@ export const getMatchBySummonerName = async (summonerName: string) => {
   const response = await axios.get(`${baseURL}/matchesInfo/${summonerName}`);
   return response.data;
 };
+
 export const getSummonerInfoBySummonerName = async (summonerName: string) => {
   const response = await axios.get(`${baseURL}/summonerInfo/${summonerName}`);
   return response.data;
