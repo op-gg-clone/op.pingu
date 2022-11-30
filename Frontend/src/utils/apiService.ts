3070d6fec81d1c222add61b9df38cdbfcd8ef162
Velo248 <jhpark4884@gmail.com>
2022-11-30 15:25:00 +0900

    add (service): api호출 함수

---
+++ Frontend/src/utils/apiService.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const baseURL: string = 'http://localhost:3000/records';
+
+export const getMatchBySummonerName = async (summonerName: string) => {
+  const response = await axios.get(`${baseURL}/matchesInfo/${summonerName}`);
+  return response.data;
+};
+export const getSummonerInfoBySummonerName = async (summonerName: string) => {
+  const response = await axios.get(`${baseURL}/summonerInfo/${summonerName}`);
+  return response.data;
+};
