81140b0871fcbc673decafbb049edd711e3fa938
Daekuen <86470018+Daekuen@users.noreply.github.com>
2022-12-02 03:32:34 +0900

    fix spell, perk url
    fix spell, perk url

--- Backend/src/records/records.service.ts
+++ Backend/src/records/records.service.ts
@@ -8,6 +8,47 @@ import { perks } from './records.perkList';
 export class RecordsService {
   constructor(private readonly httpService: HttpService) {}
 
+  private spell = {
+    21: 'SummonerBarrier',
+    1: 'SummonerBoost',
+    14: 'SummonerDot',
+    3: 'SummonerExhaust',
+    4: 'SummonerFlash',
+    6: 'SummonerHaste',
+    7: 'SummonerHeal',
+    13: 'SummonerMana',
+    11: 'SummonerSmite',
+    12: 'SummonerTeleport',
+  };
+
+  private mainPerk = {
+    8005: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Precision/PressTheAttack/PressTheAttack.png',
+    8008: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Precision/LethalTempo/LethalTempoTemp.png',
+    8010: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Precision/Conqueror/Conqueror.png',
+    8021: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Precision/FleetFootwork/FleetFootwork.png',
+    8112: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Domination/Electrocute/Electrocute.png',
+    8124: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Domination/Predator/Predator.png',
+    8128: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Domination/DarkHarvest/DarkHarvest.png',
+    9923: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Domination/HailOfBlades/HailOfBlades.png',
+    8214: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Sorcery/SummonAery/SummonAery.png',
+    8229: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Sorcery/ArcaneComet/ArcaneComet.png',
+    8230: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Sorcery/PhaseRush/PhaseRush.png',
+    8351: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Inspiration/GlacialAugment/GlacialAugment.png',
+    8360: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Inspiration/UnsealedSpellbook/UnsealedSpellbook.png',
+    8369: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Inspiration/FirstStrike/FirstStrike.png',
+    8437: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Resolve/GraspOfTheUndying/GraspOfTheUndying.png',
+    8439: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Resolve/VeteranAftershock/VeteranAftershock.png',
+    8465: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Resolve/Guardian/Guardian.png',
+  };
+
+  private subPerk = {
+    8000: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/7201_Precision.png',
+    8100: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/7200_Domination.png',
+    8200: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/7202_Sorcery.png',
+    8300: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/7203_Whimsy.png',
+    8400: 'https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/7204_Resolve.png',
+  };
+  
   // 소환사 아이디로 전적 불러오기
   // 소환사 id => 소환사의 puuid 획득 => puuid로 최근게임 20개의 mathchId 획득 => matchId로 각 게임의 세부정보 획득 반복함
   async getMatchesBySummonerName(summonerName: string, start = 0) {
@@ -170,20 +211,14 @@ export class RecordsService {
               item4: `https://ddragon.leagueoflegends.com/cdn/${process.env.API_VER}/img/item/${summoner.item4}.png`,
               item5: `https://ddragon.leagueoflegends.com/cdn/${process.env.API_VER}/img/item/${summoner.item5}.png`,
               item6: `https://ddragon.leagueoflegends.com/cdn/${process.env.API_VER}/img/item/${summoner.item6}.png`,
-              mainPerk: `https://cdn.darkintaqt.com/lol/c-assets/perk-images/Styles/Domination/Electrocute/Electrocute.png`,
-              // {
-              //   style: summoner.perks.styles[0].style,
-              //   perk: summoner.perks.styles[0].selections[0].perk,
-              // }
-              subPerk: `https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/Precision/PressTheAttack/PressTheAttack.png`,
-              spell1: `http://ddragon.leagueoflegends.com/cdn/${process.env.API_VER}/img/spell/SummonerFlash.png`,
-              spell2: `http://ddragon.leagueoflegends.com/cdn/${process.env.API_VER}/img/spell/SummonerFlash.png`,
-              // spell1: spellData.filter((spell) => {
-              //   parseInt(spell.key) == summoner.summoner1Id;
-              // }).id,
-              // spell2: spellData.filter((spell) => {
-              //   parseInt(spell.key) == summoner.summoner2Id;
-              // }).id,
+              mainPerk: this.mainPerk[summoner.perks.styles[0].selections[0].perk],
+              subPerk: this.subPerk[summoner.perks.styles[1].style],
+              spell1: `http://ddragon.leagueoflegends.com/cdn/${
+                process.env.API_VER
+              }/img/spell/${this.spell[summoner.summoner1Id]}.png`,
+              spell2: `http://ddragon.leagueoflegends.com/cdn/${
+                process.env.API_VER
+              }/img/spell/${this.spell[summoner.summoner2Id]}.png`,
               totalDamageDealt: summoner.totalDamageDealt,
               totalDamageDealtToChampions: summoner.totalDamageDealtToChampions,
               totalDamageShieldedOnTeammates:
